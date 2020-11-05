cc49c9cb6ab44155f0898a3efd2798ffa2a01bc0
맹수연 <sooyeon030908@gmail.com>
2020-11-06 03:01:15 +0900

    오디오 자동재생 코드 수정 필요

---
+++ js/play.js
@@ -0,0 +1,7 @@
+// window.onload = function(){
+//     var audio = new Audio('audio/play_audio.mp3');
+//     audio.addEventListener('ended', function() { 
+//         this.currentTime = 0;
+//         this.play();
+//     }, false);
+// }
