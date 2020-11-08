c2ebe5a9ff4110ae6559b75e69b6dcea7a18a5d9
맹수연 <sooyeon030908@gmail.com>
2020-11-09 03:07:44 +0900

    modify play - html, js file

--- js/play.js
+++ js/play.js
@@ -1,7 +1,146 @@
-// window.onload = function(){
-//     var audio = new Audio('audio/play_audio.mp3');
-//     audio.addEventListener('ended', function() { 
-//         this.currentTime = 0;
-//         this.play();
-//     }, false);
-// }
+window.onload = function(){
+        $('input[type="image"]').click( function(e) {
+            var $this = $(e.target);
+            switch($this.attr('id')) {
+                case 'red_portion':
+                    $('#red_portion_pour').show();
+                    $('#yellow_portion_pour').hide();
+                    $('#skyblue_portion_pour').hide();
+                    $('#blue_portion_pour').hide();
+                    $('#green_portion_pour').hide();
+                    $('#deepgreen_portion_pour').hide();
+                    $('#purple_portion_pour').hide();
+                    $('#brown_portion_pour').hide();
+                    $('#white_portion_pour').hide();
+                    $('#black_portion_pour').hide();
+                        break;
+
+
+                case 'yellow_portion':
+                    $('#yellow_portion_pour').show();
+                    $('#red_portion_pour').hide();
+                    $('#skyblue_portion_pour').hide();
+                    $('#blue_portion_pour').hide();
+                    $('#green_portion_pour').hide();
+                    $('#deepgreen_portion_pour').hide();
+                    $('#purple_portion_pour').hide();
+                    $('#brown_portion_pour').hide();
+                    $('#white_portion_pour').hide();
+                    $('#black_portion_pour').hide();
+                        break;   
+
+
+                case 'skyblue_portion':
+                    $('#skyblue_portion_pour').show();
+                    $('#red_portion_pour').hide();
+                    $('#yellow_portion_pour').hide();
+                    $('#blue_portion_pour').hide();
+                    $('#green_portion_pour').hide();
+                    $('#deepgreen_portion_pour').hide();
+                    $('#purple_portion_pour').hide();
+                    $('#brown_portion_pour').hide();
+                    $('#white_portion_pour').hide();
+                    $('#black_portion_pour').hide();
+                        break;     
+
+                case 'blue_portion':
+                    $('#blue_portion_pour').show();
+                    $('#red_portion_pour').hide();
+                    $('#yellow_portion_pour').hide();
+                    $('#skyblue_portion_pour').hide();
+                    $('#green_portion_pour').hide();
+                    $('#deepgreen_portion_pour').hide();
+                    $('#purple_portion_pour').hide();
+                    $('#brown_portion_pour').hide();
+                    $('#white_portion_pour').hide();
+                    $('#black_portion_pour').hide();
+                        break;    
+
+                case 'green_portion':
+                    $('#green_portion_pour').show();
+                    $('#red_portion_pour').hide();
+                    $('#yellow_portion_pour').hide();
+                    $('#skyblue_portion_pour').hide();
+                    $('#blue_portion_pour').hide();
+                    $('#deepgreen_portion_pour').hide();
+                    $('#purple_portion_pour').hide();
+                    $('#brown_portion_pour').hide();
+                    $('#white_portion_pour').hide();
+                    $('#black_portion_pour').hide();
+                        break;     
+
+
+                case 'deepgreen_portion':
+                    $('#deepgreen_portion_pour').show();
+                    $('#red_portion_pour').hide();
+                    $('#yellow_portion_pour').hide();
+                    $('#skyblue_portion_pour').hide();
+                    $('#blue_portion_pour').hide();
+                    $('#green_portion_pour').hide();
+                    $('#purple_portion_pour').hide();
+                    $('#brown_portion_pour').hide();
+                    $('#white_portion_pour').hide();
+                    $('#black_portion_pour').hide();
+                        break;
+
+
+                case 'purple_portion':
+                    $('#purple_portion_pour').show();
+                    $('#red_portion_pour').hide();
+                    $('#yellow_portion_pour').hide();
+                    $('#skyblue_portion_pour').hide();
+                    $('#blue_portion_pour').hide();
+                    $('#green_portion_pour').hide();
+                    $('#deepgreen_portion_pour').hide();
+                    $('#brown_portion_pour').hide();
+                    $('#white_portion_pour').hide();
+                    $('#black_portion_pour').hide();
+                        break;   
+
+
+                case 'brown_portion':
+                    $('#brown_portion_pour').show();
+                    $('#red_portion_pour').hide();
+                    $('#yellow_portion_pour').hide();
+                    $('#skyblue_portion_pour').hide();
+                    $('#blue_portion_pour').hide();
+                    $('#green_portion_pour').hide();
+                    $('#deepgreen_portion_pour').hide();
+                    $('#purple_portion_pour').hide();
+                    $('#white_portion_pour').hide();
+                    $('#black_portion_pour').hide();
+                        break;     
+
+
+                case 'white_portion':
+                    $('#white_portion_pour').show();
+                    $('#red_portion_pour').hide();
+                    $('#yellow_portion_pour').hide();
+                    $('#skyblue_portion_pour').hide();
+                    $('#blue_portion_pour').hide();
+                    $('#green_portion_pour').hide();
+                    $('#deepgreen_portion_pour').hide();
+                    $('#purple_portion_pour').hide();
+                    $('#brown_portion_pour').hide();
+                    $('#black_portion_pour').hide();
+                        break;    
+
+
+                case 'black_portion':
+                    $('#black_portion_pour').show();
+                    $('#red_portion_pour').hide();
+                    $('#yellow_portion_pour').hide();
+                    $('#skyblue_portion_pour').hide();
+                    $('#blue_portion_pour').hide();
+                    $('#green_portion_pour').hide();
+                    $('#deepgreen_portion_pour').hide();
+                    $('#purple_portion_pour').hide();
+                    $('#brown_portion_pour').hide();
+                    $('#white_portion_pour').hide();
+                        break;        
+            }
+        
+            // $('#cauldron_change').show();
+            // $('#cauldron').hide();
+    });
+}
