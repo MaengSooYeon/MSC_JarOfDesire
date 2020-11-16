0d5dd79a6cb7dc6060c4287e8ce79374f73d9885
ParkJaeyeon0308 <62472505+ParkJaeyeon0308@users.noreply.github.com>
2020-11-17 02:23:07 +0900

    js commit

--- js/howplay.js
+++ js/howplay.js
@@ -1,7 +1,7 @@
-function ShowModal() { //함수 ShowPopup()으로 id = popup이 보여짐.
+function ShowModal() { //함수 ShowModal()으로 id = modal_id이 보여짐.
     $('#modal_id').show();
 }
 
-function HideModal() { //함수 HidePouup()으로 id = popup이 숨겨짐.
+function HideModal() { //함수 HideModal()으로 id = modal_id이 숨겨짐.
     $('#modal_id').hide();
 }
