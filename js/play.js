af84e6c4b2756e53dca48c91d42a96e051c6efeb
맹수연 <sooyeon030908@gmail.com>
2020-11-16 09:37:51 +0900

    delete status portion contents

--- js/play.js
+++ js/play.js
@@ -1,106 +1,74 @@
 window.onload = function(){
     $(document).ready(function(){
-        // var count=0; 
+        var count=0; 
         var score=0;
-        var portion_count;
-        var click_count=0;
+        // var portion_count;
         $('.red_portion').bind('click', function fncSearch(){
-            // portion_count=1;
-            // count++;
+            count++;
             score+=1;
-            click_count++;
             score_result(score);
-            // count_changeicon(count);
-            status_portion(portion_count, click_count);
+            count_changeicon(count);
         });
         $('.yellow_portion').bind('click', function fncSearch(){
-            // portion_count=2;
-            // count++;  
+            count++;  
             score+=2;
-            click_count++;
             score_result(score);
-            // count_changeicon(count);
-            status_portion(portion_count, click_count);
+            count_changeicon(count);
         });  
         $('.skyblue_portion').bind('click', function fncSearch(){
-            // portion_count=3;
-            // count++;  
+            count++;  
             score+=3;
-            click_count++;
             score_result(score);
-            // count_changeicon(count);
-            status_portion(portion_count, click_count);
+            count_changeicon(count);
         }); 
         $('.blue_portion').bind('click', function fncSearch(){
-            // portion_count=4;
-            // count++;  
+            count++;  
             score+=4;
-            click_count++;
             score_result(score);
-            // count_changeicon(count);
-            status_portion(portion_count, click_count);
+            count_changeicon(count);
         }); 
         $('.green_portion').bind('click', function fncSearch(){
-            // portion_count=5;
-            // count++; 
+            count++; 
             score+=5; 
-            click_count++;
             score_result(score);
-            // count_changeicon(count);
-            status_portion(portion_count, click_count);
+            count_changeicon(count);
         }); 
         $('.deepgreen_portion').bind('click', function fncSearch(){
-            // portion_count=6;
-            // count++;  
+            count++;  
             score+=6;
-            click_count++;
             score_result(score);
-            // count_changeicon(count);
-            status_portion(portion_count, click_count);
+            count_changeicon(count);
         }); 
         $('.purple_portion').bind('click', function fncSearch(){
-            // portion_count=7;
-            // count++;  
+            count++;  
             score+=7;
-            click_count++;
             score_result(score);
-            // count_changeicon(count);
-            status_portion(portion_count, click_count);
+            count_changeicon(count);
         }); 
         $('.brown_portion').bind('click', function fncSearch(){
-            // portion_count=8;
-            // count++;  
+            count++;  
             score+=8;
-            click_count++;
             score_result(score);
-            // count_changeicon(count);
-            status_portion(portion_count, click_count);
+            count_changeicon(count);
         }); 
         $('.white_portion').bind('click', function fncSearch(){
-            // portion_count=9;
-            // count++;  
+            count++;  
             score+=9;
-            click_count++;
             score_result(score);
-            // count_changeicon(count);
-            status_portion(portion_count, click_count);
+            count_changeicon(count);
         }); 
         $('.black_portion').bind('click', function fncSearch(){
-            // portion_count=10;
-            // count++;  
+            count++;  
             score+=10;
-            click_count++;
             score_result(score);
-            // count_changeicon(count);
-            status_portion(portion_count, click_count);
+            count_changeicon(count);
         });  
 
 
         $('.reset').bind('click', function fncSearch(){
-            click_count=0;  
+            count=0;
             score=0;
             score_result(score);
-            // count_changeicon(count);
 
             $('#cauldron').show();
             $('#cauldron_change').hide();
