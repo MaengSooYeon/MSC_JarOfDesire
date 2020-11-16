a8ca370c936221fdaf1862f3a8927769238a66f2
맹수연 <sooyeon030908@gmail.com>
2020-11-16 20:11:56 +0900

    add sound contents, files

---
+++ js/sound.js
@@ -0,0 +1,11 @@
+/* 선택한 아이템에 따라 효과음 부여 
+    select_sound 함수 : 아이템 선택 시 효과음 발생
+    water_flow 함수 : 포션 선택 시 pour 포션 보여줄 때 발생
+*/
+function select_sound(){
+    document.getElementById('bubble').play();
+}
+
+function water_flow(){
+    document.getElementById('water').play();
+}
